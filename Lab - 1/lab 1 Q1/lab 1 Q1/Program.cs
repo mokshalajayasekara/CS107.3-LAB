b58b248e21383fd21b2ca9ce622a98dca4498f47
Mokshala Jayasekara <135811582+mokshalajayasekara@users.noreply.github.com>
2023-07-31 11:14:06 +0530

    complete lab 1

---
+++ Lab - 1/lab 1 Q1/lab 1 Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mokshla";
+            double Btach = 22.2;
+
+            Console.WriteLine("Your name is" + Name);
+            Console.WriteLine("Your Batch is" + Batch);
+
+            Console.ReadLine();
+        }
+    }
+}
